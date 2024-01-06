772b5204a214c21fe6c00a3c55efaa0ca18d2a05
ryo.dev <151644719+haryodarma@users.noreply.github.com>
2024-01-06 13:52:48 +0800

    Update Project UAS Pemrograman Mobile 1

--- UAS-Pemrograman_Mobile_1/inventory-book/bin/bookcasier.dart
+++ UAS-Pemrograman_Mobile_1/inventory-book/bin/bookcasier.dart
@@ -6,10 +6,13 @@ void main() {
   List<BookSelf> book = [];
   bool loop = true;
   print('''
+
+
 Inventory-Book.app
-created by : Haryo Darma Putra
+created by : Haryo Darma Putra & Baiq Fadilatul Islamiah
 version    : 1.0.0
 
+"You can access the source code on GitHub of ryo.dev or visit instagram of @ryodrma_"
 
 ''');
   while (loop) {
@@ -19,32 +22,32 @@ version    : 1.0.0
       print('===================================');
       print('|| 1. Add A Book                 ||');
       print('|| 2. Show All Book in Inventory ||');
-      print('|| 3. Search Book by Tittle      ||');
-      print('|| 4. Search Book by Stock       ||');
-      print('|| 5. Remove The Book            ||');
-      print('|| 6. Update Stock               ||');
+      print('|| 3. Update Stock               ||');
+      print('|| 4. Remove The Book            ||');
+      print('|| 5. Search Book by Tittle      ||');
+      print('|| 6. Search Book by Stock       ||');
       print('|| 0. Quit From Application      ||');
       print('===================================');
       stdout.write('Menu : ');
       var menu = int.parse(stdin.readLineSync()!);
       switch (menu) {
         case 1:
-          add(book);
+          add(book); // Create
           break;
         case 2:
-          show(book);
+          show(book); // Read
           break;
         case 3:
-          sBTittle(book);
+          updateStock(book); // Update
           break;
         case 4:
-          sBStock(book);
+          delete(book); // Delete
           break;
         case 5:
-          delete(book);
+          sBTittle(book); // Extra Function : Search
           break;
         case 6:
-          updateStock(book);
+          sBStock(book); // Extra Function : Search
           break;
         case 0:
           loop = false;
